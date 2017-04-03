bdf9a09e2065d2377c9d5262b879fa29fe155caf
jonas <jonas@luethke.net>
2017-04-03 23:19:08 +0800

    Update 2017-04-03T15:18:37.285Z

--- en/resize-images.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd new_images
-mogrify -auto-orient -resize 1400x1400 -quality 90 -path ../assets/images/posts *.JPG
-mogrify -auto-orient -resize 800x800 -quality 90 -path ../assets/images/posts/thumbnail *.JPG
